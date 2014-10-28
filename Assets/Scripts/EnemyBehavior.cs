e1107faf6dc291382986351ec89f66064e9725d6
Ian <iswain@ccs.neu.edu>
2014-10-27 20:25:20 -0400

    Got basic enemy spawning with correct spawn rotation.

--- Assets/Scripts/EnemyBehavior.cs
+++ Assets/Scripts/EnemyBehavior.cs
@@ -1,11 +1,19 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class EnemyBehavior : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+	void Awake () {
+        GameObject circ = GameObject.FindWithTag("playarea");
+        GameObject transObj = new GameObject("Transformer");
+        transObj.transform.position = circ.transform.position;
+        transObj.transform.position = new Vector3 
+        (transObj.transform.position.x, transObj.transform.position.y, 1);
+	    transform.LookAt(transObj.transform);
+
+        if(gameObject.tag.Equals("triangle")) {
+            transform.Rotate(180, 0, 0);
+        }
 	}
 	
 	// Update is called once per frame
--- Assets/Scripts/EnemyControl.cs
+++ Assets/Scripts/EnemyControl.cs
@@ -1,15 +1,51 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class EnemyControl : MonoBehaviour {
 
-	// Use this for initialization
+    public GameObject triEnemy;
+    public GameObject squEnemy;
+    public GameObject cirEnemy;
+
+    public float spawnRate = 1.0f;
+    private float lastSpawn = 0.0f;
+
+    private int enSpawn;
+
 	void Start () {
-	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+	       if(Time.time - lastSpawn > spawnRate) {
+            lastSpawn = Time.time;
+            Instantiate(randEnemy(), randPoint(), Quaternion.identity);
+           }
 	}
+
+    GameObject randEnemy() {
+        int r = Random.Range(0,3);
+        switch (r) {
+            case 0:
+                return cirEnemy;
+            case 1:
+                return triEnemy;
+            case 2:
+                return squEnemy;
+            default:
+                Debug.Log("Randomization error");
+                return null;
+        } 
+    }
+
+    Vector3 randPoint() {
+        float r = gameObject.GetComponent<SphereCollider>().radius;
+        r = r * transform.localScale.x;
+
+        float angle = Random.Range(0, Mathf.PI * 2);
+
+        Vector3 point = new Vector3(r * Mathf.Cos(angle), r * Mathf.Sin(angle), 1);
+
+        return point;
+    }
 }
